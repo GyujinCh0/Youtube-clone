2873394eaa53cd2e046a7367c565b954b3cfddc7
Gyujin Cho <och1939420@gmail.com>
2022-01-31 17:16:03 +0900

    Feat: 뷰 파일 생성 및 뷰 엔진 셋업(pug)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("home");
 export const see = (req, res) => res.send("Watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
