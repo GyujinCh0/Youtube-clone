b8f44bc9ad2b49998e22f524237b47f597623fcf
Gyujin Cho <och1939420@gmail.com>
2022-01-31 18:54:24 +0900

    Feat: DB 연결(mongoDB, mongoose)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -14,6 +15,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
