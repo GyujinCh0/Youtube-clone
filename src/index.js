fcf7228e77de8a7f81e4d22ee8a6bcfa98e82d79
Gyujin Cho <och1939420@gmail.com>
2022-01-31 11:37:24 +0900

    Feat: 라우터 생성(global, user, video)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/UserRouters";
+import videoRouter from "./routers/video Router";
 
 const PORT = 4000;
+
 const app = express();
 
-const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`)
+const logger = morgan("dev");
+
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("videos", videoRouter);
+
+const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
