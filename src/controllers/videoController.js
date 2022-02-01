f4ea2ff5d53b47970deb1dbf64af388d0b8b7218
Gyujin Cho <och1939420@gmail.com>
2022-02-01 09:49:02 +0900

    Feat: 뷰 생성 및 변경(upload, footer)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,5 +7,5 @@ export const home = async (req, res) => {
 export const see = (req, res) => res.send("Watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
+export const upload = (req, res) => res.render("upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
