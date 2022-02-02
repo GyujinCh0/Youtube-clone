660f4ae337f8b3d09774e6fe2be7d340e403b4f6
Gyujin Cho <och1939420@gmail.com>
2022-02-02 20:04:44 +0900

    Feat: Add controller(deleteVideo), view(watch) for delete video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -60,4 +60,8 @@ export const postUpload = async (req, res) => {
   }
 };
 
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
