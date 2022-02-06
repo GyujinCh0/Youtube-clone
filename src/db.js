88c2c49f185162a09a323d5d1b221e0b16acf0de
Gyujin Cho <och1939420@gmail.com>
2022-02-06 21:20:07 +0900

    Feat: Update postJoin,join(view) for join

--- src/db.js
+++ src/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 mongoose.connect("mongodb://127.0.0.1:27017/youtube", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
