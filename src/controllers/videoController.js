b1917c4d86b0f57fa2ee5bed9658930670476f42
Gyujin Cho <och1939420@gmail.com>
2022-02-08 23:11:42 +0900

    Feat:Update get&postEdit,postUpload,postJoin for status code

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -14,7 +14,7 @@ export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
@@ -23,7 +23,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
@@ -53,7 +53,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(404).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
