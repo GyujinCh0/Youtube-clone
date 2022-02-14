4248b230e19b7475941343c4498d563cdc6ecca0
Gyujin Cho <och1939420@gmail.com>
2022-02-15 08:43:05 +0900

    Add see, profile for my profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -28,6 +28,6 @@ userRouter
   .post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
-userRouter.get(":id", see);
+userRouter.get("/:id", see);
 
 export default userRouter;
