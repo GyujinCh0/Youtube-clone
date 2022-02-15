d04259b0ebb0d26b0ac35a5cfd2ce08ed8370664
Gyujin Cho <och1939420@gmail.com>
2022-02-16 07:57:29 +0900

    Add webpack.config,nodemon.json for managing assets

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,8 +9,6 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id).populate("owner");
-  const video = await Video.findById(id);
-  const owner = await User.findById(video.owner);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
