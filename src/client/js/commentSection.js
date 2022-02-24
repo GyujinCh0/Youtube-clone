87347c640851fd66ebfdf766d71cedfa44b803d1
Gyujin Cho <och1939420@gmail.com>
2022-02-24 09:57:01 +0900

    Update commentSection,apiRouter for comment api routing

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,24 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  if (text === "") {
+    return;
+  }
+
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
 };
 
+if (form) {
 form.addEventListener("submit", handleSubmit);
+}
--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
