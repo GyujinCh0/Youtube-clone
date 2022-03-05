48fe5ea86da86dc7482813e1feeaaac1219fe803
Gyujin Cho <och1939420@gmail.com>
2022-03-06 00:39:00 +0900

    Fix middlware for aws S3

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,7 +2,7 @@ import multer from "multer";
 import multerS3 from "multer-s3"
 import aws from "aws-sdk";
 
-const s3 = new aws.s3({
+const s3 = new aws.S3({
   credentials:{
     accessKeyId: process.env.AWS_ID,
     secretAccessKey:process.env.AWS_SECRET
